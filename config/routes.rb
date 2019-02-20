71238eedbfface0710688456055ac20be04da865
Emma Scanlon <emmascanlon4@gmail.com>
2019-02-19 20:29:32 -0700

    add static home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'teams#index'
+  root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+
   resources:teams
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
