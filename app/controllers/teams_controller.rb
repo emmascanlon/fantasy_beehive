8718fcc4315db295c5c092b6b652a38bfdf2bb9b
Emma Scanlon <emmascanlon4@gmail.com>
2019-02-19 18:06:33 -0700

    add new

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -13,4 +13,21 @@ class TeamsController < ApplicationController
 
   def edit
   end
+
+  def create
+    @team = Team.new(teams_params)
+    
+    if @team.save
+      redirect_to teams_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def teams_params
+      params.require(:team).permit(:name, :sport, :season, :year, :player1, :player2, :player3, :player4, :player5, :player6, :player7, :player8, :player9, :player10, :player11, :player12, :player13, :player14, :player15)
+    end
+
 end
+
